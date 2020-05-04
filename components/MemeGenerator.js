6aaf21098de5f0786a8cee888e20c6fbaab215ca
Simon Lawrenson <simon@optimising.com.au>
2020-05-04 21:14:42 +1000

    backup commit

---
+++ components/MemeGenerator.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react"
+
+class MemeGenerator extends Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return(
+      <h2>MemeGenerator</h2>
+    )
+  }
+}
+
+export default MemeGenerator
